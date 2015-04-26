b7c0551150da6c212314a609dd122b04c88bcb6e
Fsoya <dshsun@gmail.com>
2015-04-26 11:14:36 +0800

    autologin

--- src/main/webapp/resources/js/tdct/main.js
+++ src/main/webapp/resources/js/tdct/main.js
@@ -9,15 +9,13 @@ login.controller("LoginController",function($scope,$http) {
     $http.post("/checkLogin").success(function(obj){
        if(obj.status=="T"){
            $scope.haslogin = true;
-           $scope.comeInfo = "欢迎：" + obj.data;
+           $scope.comeInfo = obj.data;
        } else{
            $scope.haslogin = false;
        }
-        console.log($scope.haslogin);
     });
 
     var form = {};
-
     $scope.form = form;
 
     $scope.login = function(isValid){
@@ -25,9 +23,12 @@ login.controller("LoginController",function($scope,$http) {
             $http.post("/login", $scope.form).success(function (obj) {
                 if(obj.status=="T") {
                     $.cookie("userName",obj.data.userName,{expires: 7});
-                    $.cookie("token",obj.data.token,{expires: 7});
                     $.cookie("nickName",obj.data.nickName,{expires: 7});
-                    $scope.comeInfo = "欢迎：" + obj.data.nickName;
+                    $scope.comeInfo = obj.data.nickName;
+                    if(form.isRemember){
+                        $.cookie("token",obj.data.token,{expires: 7});
+                    }
+                    $('#loginWin').modal('hide');
                     $scope.haslogin = true;
                 }else{
                     $scope.haslogin = false;
@@ -44,7 +45,6 @@ login.controller("LoginController",function($scope,$http) {
     $scope.toLogoff = function(){
         $http.post("/logoff").success(function(obj){
             if(obj.status=="T"){
-                $.cookie("userName", '', { expires: -1 });
                 $.cookie("token", '', { expires: -1 });
                 $.cookie("nickName", '', { expires: -1 });
                 location.href = "/"
