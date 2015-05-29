2b6ff40a36023d2b7809e976d95fb5d98771066f
Fsoya <dshsun@gmail.com>
2015-05-29 19:44:44 +0800

    base data && redis

--- src/main/test/RunData.java
+++ src/main/test/RunData.java
@@ -14,7 +14,22 @@ public class RunData {
         StringRedisTemplate redisTemplate = (StringRedisTemplate)ctx.getBean("redisTemplate");
 
         BoundHashOperations<String,String,String> gametype = redisTemplate.boundHashOps("gameType");
-        gametype.put("AVG  ","冒险");
+        gametype.put("ACT","动作游戏");
+        gametype.put("AVG","冒险游戏");
+        gametype.put("FPS","第一人称视点射击游戏");
+        gametype.put("FTG","格斗游戏");
+        gametype.put("MUG","音乐游戏");
+        gametype.put("PUZ","益智类游戏");
+        gametype.put("RAC","赛车游戏");
+        gametype.put("RPG","角色扮演游戏");
+        gametype.put("RTS","即时战略游戏");
+        gametype.put("SLG","模拟/战棋式战略游戏");
+        gametype.put("SPG","体育运动游戏");
+        gametype.put("STG","射击游戏");
+        gametype.put("TAB","桌面游戏");
+        gametype.put("ETC","其他类型游戏");
+        gametype.put("AVG/ADV","恋爱养成游戏");
+
         Map<String, String> entries = gametype.entries();
     }
 
